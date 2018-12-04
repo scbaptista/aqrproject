e988bc35e9664f8668e8aa574015bf150486c300
Sandra <Sandra@DESKTOP-FKIT67E>
2018-12-04 20:31:30 +0000

    Signed-off-by: Sandra <Sandra@DESKTOP-FKIT67E>

--- arqproject/src/arqproject/utils/ConnectDb.java
+++ arqproject/src/arqproject/utils/ConnectDb.java
@@ -6,42 +6,7 @@ import com.mongodb.client.MongoDatabase;
 
 public class ConnectDb {
 	
-//	public String conect(Integer op, String list, String tabela){
-//		  
-//		
-//		MongoDatabase database = getCon();
-//	      
-//	      for (String name : database.listCollectionNames()) { 
-//	          System.out.println("tableName: " + name); 
-//	       } 
-//	      
-//	      // Creating a collection 
-//	      System.out.println("Collection created successfully"); 
-//
-//	      // Retieving a collection
-//	      MongoCollection<Document> collection = database.getCollection(tabela); 
-//	      System.out.println("Collection myCollection selected successfully"); 
-//	      String result = null;
-//	      
-//	      switch (op) {
-//			case 1:
-//				result = doInsert(list, collection);
-//				break;
-//			case 2:
-//				result = doSelect(list, collection);
-//				break;
-//				
-//			case 10:
-//				result = deleteTable(collection);
-//				break;
-//			default:
-//				break;
-//		  }
-//
-//
-//	    return result;      
-//	    	
-//	}
+
 
 	@SuppressWarnings({ "resource", "unused" })
 	public MongoDatabase getCon() {
@@ -50,8 +15,7 @@ public class ConnectDb {
 	     
 	      // Creating Credentials 
 	      MongoCredential credential; 
-	      credential = MongoCredential.createCredential("sampleUser", "myDb", 
-	         "password".toCharArray()); 
+	      credential = MongoCredential.createCredential("sampleUser", "myDb",  "password".toCharArray()); 
 	      System.out.println("Connected to the database successfully");  
 	      
 	      // Accessing the database 
@@ -60,62 +24,5 @@ public class ConnectDb {
 		return database;
 	}
 
-//	private String doSelect(String list, MongoCollection<Document> collection) {
-//		 String result = null;
-//		 FindIterable<Document> iterDoc = collection.find();
-//		    String [] d = list.split(";");
-//		    for (Document iterable_element : iterDoc) {
-//		    	if(iterable_element.containsValue(d[0]) && iterable_element.containsValue(d[1])){
-//		    		   result = iterable_element.toJson(); 
-//		    	}
-//		    }
-//		
-//		    System.out.println(result);
-//		return result;
-//
-//	}
-//
-//	private String doInsert(String list, MongoCollection<Document> collection) {
-//		String msg = null;
-//	      @SuppressWarnings("static-access")
-//		Document document = new Document("title", "MongoDB") 
-//	    .parse(list);
-//	    collection.insertOne(document); 
-//	    
-//	    FindIterable<Document> iterDoc = collection.find();
-//	    
-//	    for (Document iterable_element : iterDoc) {
-//	    	 if(iterable_element.toJson().equals(document.toJson())){
-//	    		 // Creating a collection 
-//	   	      	System.out.println("Collection created successfully");
-//	   	      	msg = "Collection created successfully"; 
-//	    	 }
-//	    }
-//	          
-//		
-//		return msg;
-//		// TODO Auto-generated method stub
-//		
-//	}
-	
-//	private String deleteTable(MongoCollection<Document> collection){
-//		String msg = null;
-//		
-//		System.out.println("ola");
-//		
-//		System.out.println(collection.getNamespace().getCollectionName());
-//	     
-//	     collection.drop();
-//	     
-//	     Boolean drop = (getCon().listCollectionNames().equals(collection.getNamespace().getCollectionName()));
-//	     
-//	     if(!drop){
-//	    	 msg = "sucesso";
-//	     }else{
-//	    	 msg = "erro";
-//	     }
-//	     
-//		return msg;
-//	}
 }
 
